d7d5a1a0e3afa55ed15573df43c215755d217bc6
HenryZhou42 <59575952+HenryZhou42@users.noreply.github.com>
2020-09-16 20:43:40 -0600

    1

---
+++ ChinookDemo/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChinookSystem.DAL;
+
+#region Additional Namespaces
+using ChinookSystem.Entities;
+using ChinookSystem.ViewModels;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        //public List<SelectionList> Artists_List()
+        //{
+        //    using (var context = new ChinookSystemContext())
+        //    {
+        //        return context.Artists.ToList();
+        //    }
+        //}
+    }
+}
