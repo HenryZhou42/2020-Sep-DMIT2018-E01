93960286793cf9888ff13345191b1d322dc1dfd6
HenryZhou42 <59575952+HenryZhou42@users.noreply.github.com>
2020-09-21 20:13:37 -0600

    Create the List FindById, Add, update and delete for an ODS crud

--- ChinookDemo/ChinookSystem/BLL/AlbumController.cs
+++ ChinookDemo/ChinookSystem/BLL/AlbumController.cs
@@ -5,11 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using ChinookSystem.Entities;
 
 namespace ChinookSystem.BLL
 {
+    [DataObject]
     public class AlbumController
     {
+        #region Queries
         public List<AlbumArtist> Album_FindByArtist(int artiistid)
         {
             using (var context = new ChinookSystemContext())
@@ -27,5 +31,76 @@ namespace ChinookSystem.BLL
                 return results.ToList();
             }
         }
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public AlbumItem Album_FindByID( int albumid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                var results = (from k in context.Albums
+                              where k.AlbumId == albumid
+                              select new AlbumItem()
+                              {
+                                  Title = k.Title,
+                                  ReleaseYear = k.ReleaseYear,
+                                  ReleaseLabel = k.ReleaseLabel,
+                                  AlbumId = k.AlbumId,
+                                  ArtistId = k.ArtistId
+                              }).FirstOrDefault();
+                return results;
+            }
+        }
+        #endregion
+        #region CRUD: Add, Update and Delete
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public void Album_Add(AlbumItem item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                Album newItem = new Album
+                {
+                    //pkey is identity, not needed
+                    Title = item.Title,
+                    ArtistId = item.ArtistId,
+                    ReleaseLabel = item.ReleaseLabel,
+                    ReleaseYear = item.ReleaseYear
+                };
+                context.Albums.Add(newItem);
+                context.SaveChanges();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update, false)]
+        public void Album_Update(AlbumItem item)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                Album newItem = new Album
+                {
+                    //pkey is identity, not needed
+                    AlbumId = item.AlbumId,
+                    Title = item.Title,
+                    ArtistId = item.ArtistId,
+                    ReleaseLabel = item.ReleaseLabel,
+                    ReleaseYear = item.ReleaseYear
+                };
+                context.Entry(newItem).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        [DataObjectMethod(DataObjectMethodType.Delete,false)]
+        public void Album_Delete(AlbumItem item)
+        {
+            Album_Delete(item.AlbumId);
+        }
+        public void Album_Delete(int albumid)
+        {
+            using (var context = new ChinookSystemContext())
+            {
+                var exists = context.Albums.Find(albumid);
+                context.Albums.Remove(exists);
+                context.SaveChanges();
+            }
+        }
+        #endregion
     }
 }
--- ChinookDemo/ChinookSystem/ViewModels/AlbumItem.cs
+++ ChinookDemo/ChinookSystem/ViewModels/AlbumItem.cs
@@ -6,16 +6,14 @@ using System.Threading.Tasks;
 
 namespace ChinookSystem.ViewModels
 {
-    public class AlbumArtist
+    public class AlbumItem
     {
         public string Title { get; set; }
         public int ReleaseYear { get; set; }
         public string ReleaseLabel { get; set; }
         public int AlbumId { get; set; }
 
-
-        //this is only in this view to allow for ODS view
-        // on filling a DDL on a grid view
+        
         public int ArtistId { get; set; }
     }
 }
